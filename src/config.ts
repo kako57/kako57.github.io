cf59946862cc918a6e27c035de8149bceb49a453
Dre Cura <71569154+kako57@users.noreply.github.com>
2025-07-12 23:21:46 -0400

    allow visitors to change colors

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 	themeColor: {
 		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		// hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: true, // Hide the theme color picker for visitors
+		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: false,
