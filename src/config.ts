d00f221eaf0224e25e29820163fb002434b75141
Dre Cura <71569154+kako57@users.noreply.github.com>
2025-08-08 13:33:59 -0400

    fix license fr fr

--- src/config.ts
+++ src/config.ts
@@ -79,8 +79,8 @@ export const profileConfig: ProfileConfig = {
 
 export const licenseConfig: LicenseConfig = {
 	enable: true,
-	name: "CC BY-NC-SA 4.0",
-	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+	name: "CC BY-NC 4.0",
+	url: "https://creativecommons.org/licenses/by-nc/4.0/",
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
